a0020c5b28a5e4e94977af5a08ea7bfcc47a2b24
carlacrude@gmail.com <carlacrude@gmail.com>
2018-07-02 16:00:29 -0300

    adição terceira tarefa

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,14 @@ class RecipesController < ApplicationController
     id = params[:id]
     @recipe = Recipe.find(id)
   end
+  
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to recipe_url(@recipe)
+  end
 end
