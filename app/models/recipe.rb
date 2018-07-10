62bb48eb6471d305a035a973ce6b44eb67913998
Carla Carla <carlacristinecrudedossanto@correio.itau.com.br>
2018-07-10 09:41:03 -0300

    Usuário envia foto da receita

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :cuisine
   belongs_to :recipe_type
-
   validates :title, :difficulty, :cook_time, :ingredients, :cook_method, presence: true
+  
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
