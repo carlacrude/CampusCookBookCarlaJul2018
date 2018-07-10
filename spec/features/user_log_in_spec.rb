ff0d6e4611abe0bea3dae3738b71cf446b76ef4e
Carla Carla <carlacristinecrudedossanto@correio.itau.com.br>
2018-07-10 12:14:00 -0300

    login sem sucesso

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -17,4 +17,19 @@ feature 'User login' do
     expect(page).to have_content("Olá #{user.email}")  
   end
 
+  scenario 'does not see Entrar link' do
+    user = User.create(email: 'testeimersao@gmail.com', password: 'teste12345')
+
+    visit root_path
+    click_on 'Login'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+
+    click_on 'Entrar'
+
+    expect(current_path).to eq(root_path)
+    expect(page).not_to have_content("Entrar")  
+  end
+  
 end
